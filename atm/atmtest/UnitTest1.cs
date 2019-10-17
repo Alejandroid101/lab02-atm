92f7343aefa68c610eea13fde5d14ff6fc0f2e6f
Alejandroid101 <kik.rdl3@gmail.com>
2019-10-16 18:01:11 -0700

    Withdraw Method done and working, also first test added and passed.

--- atm/atmtest/UnitTest1.cs
+++ atm/atmtest/UnitTest1.cs
@@ -1,14 +1,15 @@
 using System;
 using Xunit;
+using static atm.Program;
 
 namespace atmtest
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void Testicle1()
         {
-
+            Assert.Equal(6000, Deposit(1000));
         }
     }
 }
